4a139075cd3b479f1af7bff7bb7e7842a5aff1d3
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2023-09-27 09:28:45 -0300

    continuing

--- projeto-integrador/src/App.jsx
+++ projeto-integrador/src/App.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react'
 import Spline from '@splinetool/react-spline';
 import styles from './style';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden"> 
@@ -12,6 +13,19 @@ const App = () => (
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${$styles.boxWidth}`}>
         Hero
+      </div>
+
+      <div className={`bg-primary ${stles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
       </div>
     </div>
   </div>
