36402030ca57911fb30f19e64adbe5fa591d8cc0
Giovanna Alves Fiuza <37076111837>
2023-10-17 16:47:04 -0300

    ---

--- projeto-integrador/src/App.jsx
+++ projeto-integrador/src/App.jsx
@@ -1,38 +1,44 @@
 import Spline from '@splinetool/react-spline';
-import styles from './style';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import styles from "./style";
+import Billing from "./components/Billing"
+import Business from "./components/Business"
+import CardDeal from "./components/CardDeal"
+import Clients from "./components/Clients"
+import CTA from "./components/CTA"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Stats from "./components/Stats"
+import Testimonials from "./components/Testimonials"
+import Hero from "./components/Hero"
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden"> 
+  <div className="bg-neutral-950 w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${$styles.boxWidth}`}>
-        Hero
-      </div>
 
-      <div className={`bg-primary ${stles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
-        </div>
+    <div className={`bg-neutral-950 ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-neutral-950 ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
-    /* <Spline scene="https://prod.spline.design/pvRP-ONEsYV3HJdr/scene.splinecode" style={{width:'80%', alignSelf: 'flex-end'}}/> */
-
 );
-  
 
-
-export default App
+export default App;
+/* <Spline scene="https://prod.spline.design/pvRP-ONEsYV3HJdr/scene.splinecode" style={{width:'80%', alignSelf: 'flex-end'}}/> */
